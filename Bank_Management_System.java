a319baa9de1c92f4900c957e4fe2f911faf49dd2
Kamohelo M. <142387996+Kamohelo1566@users.noreply.github.com>
2025-08-06 17:44:14 +0200

    Update Bank_Management_System.java

--- Bank_Management_System.java
+++ Bank_Management_System.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-
-// The main method must be in a class named "Main".
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
